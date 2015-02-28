2b9b736b11b302ba1f04e34e0065541840b56193
Mike Gallagher <gallaghermikey@gmail.com>
2015-02-27 20:48:43 -0700

    Dates now in strings in an array and using separate min and max temperatures

--- Assignment 5/ViewController.m
+++ Assignment 5/ViewController.m
@@ -14,7 +14,10 @@
 @property (nonatomic) CGFloat latitude;
 @property (nonatomic) CGFloat longitude;
 @property (nonatomic) NSString* cityName;
-@property (nonatomic, strong) NSMutableArray *temperatures;
+@property (nonatomic, strong) NSArray *maxTemps;
+@property (nonatomic, strong) NSArray *minTemps;
+@property (nonatomic, strong) NSArray *unixDates;
+@property (nonatomic, strong) NSMutableArray *actualDates;
 @property (nonatomic, strong) NSMutableArray *weatherIcons;
 @property (nonatomic, strong) UIActivityIndicatorView *spinner;
 @property (nonatomic, strong) CLLocationManager *locationManager;
@@ -54,44 +57,39 @@
 
     NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:
         ^(NSData *data, NSURLResponse *response, NSError *error) {
+            // Begin magical block lol
             self.jsonResponse = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
-
             self.cityName = [self.jsonResponse valueForKeyPath:@"city.name"];
+            self.maxTemps = [self.jsonResponse valueForKeyPath:@"list.temp.max"];
+            self.minTemps = [self.jsonResponse valueForKeyPath:@"list.temp.min"];
             self.weatherIcons = [[NSMutableArray alloc] init];
-            self.temperatures = [[NSMutableArray alloc] init];
-//             NSArray* weatherIDs = [[[[self.jsonResponse valueForKeyPath:@"list.weather"] firstObject] objectForKey:@"icon"] integerValue];
-             NSMutableArray *weatherData = [self.jsonResponse objectForKey:@"list"];
-            NSDictionary *indexDict = [weatherData objectAtIndex:0];
-             NSArray *weatherDataArr = [weatherData valueForKeyPath:@"icon"];
-            // NSDictionary *firstItem = [weatherData firstObject];
-            // NSDictionary *weatherIcon = [weatherData valueForKey:@"icon"];
-            
-            // NSString *icon = [weatherDictionaryData objectForKey:@"icon"];
-            self.temperatures = [self.jsonResponse valueForKeyPath:@"list.temp.day.0"];
-
-            // NSDictionary *testing = [self.jsonResponse valueForKeyPath:@"list.weather.icon"];
-            // NSArray *hellooo = [[[_jsonResponse valueForKey:@"list"] valueForKey:@"weather"] valueForKey:@"id"];
             for (NSDictionary *listItems in self.jsonResponse[@"list"])
             {
-                // NSLog(@"id: %@", [listItems valueForKeyPath:@"weather.id"][0]);
                 [self.weatherIcons addObject:[listItems valueForKeyPath:@"weather.icon"][0]];
-                // NSLog(@"temp: %@", [listItems valueForKeyPath:@"temp.day"]);
-                // [self.temperatures addObject:[listItems valueForKeyPath:@"temp.day"]];
             }
+            NSArray *unixDates = [self.jsonResponse valueForKeyPath:@"list.dt"];
+            [self convertDates:unixDates];
+            
+            // End block
     }];
     [dataTask resume];
 }
 
+-(void)convertDates:(NSArray*) dates
+{
+    self.actualDates = [[NSMutableArray alloc] init];
+    for (id day in dates)
+    {
+        NSDate *unixTime = [NSDate dateWithTimeIntervalSince1970:[day intValue]];
+        NSDateFormatter *dateFormatter = [NSDateFormatter new];
+        [dateFormatter setDateFormat:@"EEEE"];
+        NSString *dateString = [dateFormatter stringFromDate:unixTime];
+        [self.actualDates addObject:dateString];
+    }
+}
+
 -(void)setUpLocationGetter
 {
-//    self.latitude = [[UILabel alloc] initWithFrame:CGRectMake(20, 50, 200, 40)];
-//    self.latitude.text = @"Latitude:";
-//
-//    [self.view addSubview:self.latitude];
-//    self.longitude = [[UILabel alloc] initWithFrame:CGRectMake(20, 100, 200, 40)];
-//    self.longitude.text = @"Longitude:";
-//    [self.view addSubview:self.longitude];
-    
     //  ------------------- Spinner --------------------
     self.spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
     self.spinner.frame = CGRectMake(200, 150, 50, 50);
